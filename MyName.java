d6a32f4dca90a71d3eb9596f6d9bd2467e45e9fc
areddyalaric <53569096+areddyalaric@users.noreply.github.com>
2019-08-01 18:57:54 +0530

    Welcome comment

--- MyName.java
+++ MyName.java
@@ -3,6 +3,7 @@ public static void main(String s){
 System.out.println("My name");
   System.out.println("Another name");
   System.out.println("Thank you");
+  System.out.println("Welcome");
 }
 
 }
